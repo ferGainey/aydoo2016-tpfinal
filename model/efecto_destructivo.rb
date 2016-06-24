0cbbefd75434bf897728375c52ea31b78892fc90
ferGainey <fernandogainey@hotmail.com>
2016-06-24 01:04:12 -0300

    el EfectoDestructivo se puede aplicar tambien de forma porcentual

--- model/efecto_destructivo.rb
+++ model/efecto_destructivo.rb
@@ -5,7 +5,8 @@ class EfectoDestructivo
   attr_reader :danio_a_provocar
 
   def initialize(danio_a_provocar)
-  	if danio_a_provocar > 0
+  	danio_recibido = danio_a_provocar.to_s.gsub(/%/, "")
+  	if danio_recibido.to_f > 0
   	  @danio_a_provocar = danio_a_provocar
   	else
   	  raise DestruccionNegativaException, "No se pueda aplicar un danio negativo"
@@ -13,7 +14,11 @@ class EfectoDestructivo
   end
 
   def aplicar_efecto(objeto_espacial)
-    objeto_espacial.vida -= @danio_a_provocar
+    if @danio_a_provocar.to_s.include? "%"
+      objeto_espacial.vida = objeto_espacial.vida * ((@danio_a_provocar.to_s.gsub(/%/, "").to_f)/100)
+    else
+      objeto_espacial.vida -= @danio_a_provocar.to_f
+    end
   end
   
 end
--- spec/efecto_destructivo_spec.rb
+++ spec/efecto_destructivo_spec.rb
@@ -9,14 +9,25 @@ describe 'EfectoDestructivo' do
     vida_esperada = 80    
     masa_esperada = 1000
     objeto_espacial = ObjetoEspacial.new(vida_inicial, masa_esperada)
-    efecto_destructivo = EfectoDestructivo.new(20)
+    efecto_destructivo = EfectoDestructivo.new("20")
     efecto_destructivo.aplicar_efecto(objeto_espacial)
     expect(objeto_espacial.vida).to eq vida_esperada
     expect(objeto_espacial.masa).to eq masa_esperada
   end
 
-it 'debe lanzar una excepcion si se inicializa con danio negativo' do
+  it 'debe lanzar una excepcion si se inicializa con danio negativo' do
     expect{EfectoDestructivo.new(-30)}.to raise_exception(DestruccionNegativaException)
   end
+
+  it 'el EfectoDestructivo se puede indicar de forma porcentual' do
+    vida_inicial = 200
+    vida_esperada = 100    
+    masa_esperada = 1000
+    objeto_espacial = ObjetoEspacial.new(vida_inicial, masa_esperada)
+    efecto_destructivo = EfectoDestructivo.new("50%")
+    efecto_destructivo.aplicar_efecto(objeto_espacial)
+    expect(objeto_espacial.vida).to eq vida_esperada
+    expect(objeto_espacial.masa).to eq masa_esperada
+  end 
 
 end
