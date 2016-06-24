ab53cd3cdd3c11db430aa0bbe565e0358986fae2
ferGainey <fernandogainey@hotmail.com>
2016-06-24 14:30:47 -0300

    se agrego un EfectoMasa que si se le pasa un porcentaje negativo disminuye la masa en relacion al objeto al que choca

--- spec/efecto_masa_spec.rb
+++ spec/efecto_masa_spec.rb
@@ -15,4 +15,15 @@ describe 'EfectoMasa' do
     expect(objeto_generador_de_choque.masa).to eq masa_esperada
   end 
 
+  it 'el EfectoMasa disminuye la masa de un objeto en relacion al porcentaje negativo indicado de otro objeto' do
+    vida_esperada = 100    
+    masa_esperada = 1500
+    objeto_generador_de_choque = ObjetoEspacial.new(100, 2000)
+    objeto_recibidor_de_choque = ObjetoEspacial.new(80, 1000)
+    efecto_masa = EfectoMasa.new(objeto_recibidor_de_choque, "-50%")
+    efecto_masa.aplicar_efecto(objeto_generador_de_choque)
+    expect(objeto_generador_de_choque.vida).to eq vida_esperada
+    expect(objeto_generador_de_choque.masa).to eq masa_esperada
+  end 
+
 end
