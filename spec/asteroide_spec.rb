af6bc93301ff374a959738a655087646be51d373
ferGainey <fernandogainey@hotmail.com>
2016-06-23 23:04:38 -0300

    se crea el objeto Asteroide, que es un ObjetoEspacial

---
+++ spec/asteroide_spec.rb
@@ -0,0 +1,14 @@
+require 'rspec' 
+require_relative '../model/asteroide'
+
+describe 'Asteroide' do
+
+  it 'un Asteroide es un ObjetoEspacial, por lo que se crea con una cierta vida y masa' do
+    vida_esperada = 40    
+    masa_esperada = 20
+    asteroide = Asteroide.new(vida_esperada, masa_esperada)
+    expect(asteroide.vida).to eq vida_esperada
+    expect(asteroide.masa).to eq masa_esperada
+  end
+
+end
