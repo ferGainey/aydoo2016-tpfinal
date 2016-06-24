9fbacf1b3dcf087f87162f6c7f4e9e443f8fc9c3
ferGainey <fernandogainey@hotmail.com>
2016-06-24 14:28:19 -0300

    se agrego un EfectoMasa que si se le pasa un porcentaje positivo aumenta la masa en relacion al objeto al que choca

---
+++ model/efecto_masa.rb
@@ -0,0 +1,17 @@
+class EfectoMasa
+
+	def initialize(objeto_recibidor_de_choque, porcentaje_de_masa)
+	  @objeto_recibidor_de_choque = objeto_recibidor_de_choque
+	  @porcentaje_de_masa = porcentaje_de_masa
+	end
+
+	#si el porcentaje es positivo aumenta la masa, si es negativo disminuye la masa
+	def aplicar_efecto(objeto_espacial)
+	  porcentaje_de_masa_actual = @porcentaje_de_masa
+	  if(porcentaje_de_masa_actual.to_s.include? "%")
+	  	porcentaje_de_masa_actual.gsub!(/%/, "")
+	  end
+	  objeto_espacial.masa = objeto_espacial.masa + ((porcentaje_de_masa_actual.to_f/100) * @objeto_recibidor_de_choque.masa)  
+	end
+				  
+end
---
+++ spec/efecto_masa_spec.rb
@@ -0,0 +1,18 @@
+require 'rspec' 
+require_relative '../model/efecto_masa'
+require_relative '../model/objeto_espacial'
+
+describe 'EfectoMasa' do
+
+  it 'el EfectoMasa aumenta la masa de un objeto en relacion al porcentaje positivo indicado de otro objeto' do
+    vida_esperada = 100    
+    masa_esperada = 2500
+    objeto_generador_de_choque = ObjetoEspacial.new(100, 2000)
+    objeto_recibidor_de_choque = ObjetoEspacial.new(80, 1000)
+    efecto_masa = EfectoMasa.new(objeto_recibidor_de_choque, "50%")
+    efecto_masa.aplicar_efecto(objeto_generador_de_choque)
+    expect(objeto_generador_de_choque.vida).to eq vida_esperada
+    expect(objeto_generador_de_choque.masa).to eq masa_esperada
+  end 
+
+end
