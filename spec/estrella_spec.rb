ef089831bf8572ad5dd52a592f5bc297e73725e8
ferGainey <fernandogainey@hotmail.com>
2016-06-26 14:11:20 -0300

    si una estrella choca con una Nave sufre un danio destructivo del 100 porciento, mientras la Nave aumenta su vida en una cantidad igual a la vida de la estrella

--- spec/estrella_spec.rb
+++ spec/estrella_spec.rb
@@ -13,4 +13,19 @@ describe 'Estrella' do
     expect(estrella.masa).to eq masa_esperada
   end
 
+  it 'si una estrella choca con una Nave sufre un danio destructivo del 100 porciento, mientras la Nave aumenta su vida en una cantidad igual a la vida de la estrella' do
+    vida_esperada_nave = 200 
+    masa_esperada_nave = 400
+    vida_esperada_estrella = 0
+    masa_esperada_estrella = 800    
+    procesador_de_choque = Choque.new
+    nave = Nave.new(400, procesador_de_choque)
+    estrella_generadora_de_choque = Estrella.new(800, procesador_de_choque)
+    estrella_generadora_de_choque.chocar(nave)
+    expect(nave.vida).to eq vida_esperada_nave
+    expect(nave.masa).to eq masa_esperada_nave
+    expect(estrella_generadora_de_choque.vida).to eq vida_esperada_estrella
+    expect(estrella_generadora_de_choque.masa).to eq masa_esperada_estrella
+  end
+
 end
